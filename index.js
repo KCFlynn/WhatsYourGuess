6c9be467712c91b833fafa6023731b745d474d13
Sean Newman <sean.m.newman@comcast.net>
2018-06-01 13:32:08 -0700

    small changes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ window.onload = function () {
     });*/
 
     $(document).on('click', '#create', function(event){
-      addNewUser();ButtonQuit
+      addNewUser();
     }); 
 
    /* $(document).on('click', '#bet1', function(event){
