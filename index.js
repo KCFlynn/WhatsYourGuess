d00306b3c813e7513524b5da82e988069c113c96
KCFlynn <kellyflynn3@msn.com>
2018-06-01 12:30:05 -0700

    refining game more to come

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
+
 window.onload = function () {
 
     userArray.length = 0;  // clear array
     
-    $(document).on('click', '#ButtonBet', function(event){
+  /*  $(document).on('click', '#ButtonBet', function(event){
         buttonClicked();
-    });
+    });*/
 
-    $(document).on('click', '#create', function(event){
+    $(document).on('click', '#enterguess', function(event){
       addNewUser();ButtonQuit
     }); 
 
@@ -28,32 +29,32 @@ window.onload = function () {
         quit();
     });
 
-    $(document).on("pagebeforeshow", "#pickplayer",function(event){
+    $(document).on("pagebeforeshow", "#playerlist",function(event){
         createList();
     });
  
     $(document).on("pagebeforeshow", "#PickBet",function(event){
         var which = $('#IDparmHere').text();  // get the full name out of the hidden HTML
-        state.current_fullName = which;
+        state.current_playerInfo = which;
         setCurrent_index(which); // get pointer in our array of users based on fullname
         // force the mongo dollar amount to an int, not a string
-        state.current_balance = parseInt(userArray[state.current_index].MongoBalance);
-        var x = "Welcome " + state.current_fullName  + " You have $" + state.current_balance;
+        state.current_score = parseInt(userArray[state.current_index].MongoScore);
+        var x = "Welcome " + state.current_playerInfo  + " You have $" + state.current_score;
         $('#welcome').text(x);
     });
 
 
     $(document).on("pagebeforeshow", "#game",function(event){
         document.getElementById("turnCount").innerText = 0;
-        document.getElementById("balance").innerText = state.current_balance;
+        document.getElementById("score").innerText = state.current_score;
         document.getElementById("status").innerText = "Good luck!";
-        document.getElementById("ButtonBet").style.visibility = 'visible';
+        //document.getElementById("ButtonBet").style.visibility = 'visible';
     });
 
      // set up an event, if user clicks any, it writes that items data-parm into the hidden html 
     $(document).on('click', '.onePlayer', function(event){
         //$('.onePlayer').on("click", function (event) {    // this worked first time through, but not second!!
-        var parm = $(this).attr("data-parm");  // passing in the fullname
+        var parm = $(this).attr("data-parm");  // passing in the playerInfo
         //write that fullname value into the  pickbet page
         $('#IDparmHere').text(parm);
     });
@@ -66,9 +67,9 @@ window.onload = function () {
 
 var state = {
     current_index: 999,  // these values better get set before we use them
-    current_balance: 20.20,
-    current_betAmount: 0,
-    current_fullName: "?"
+    //current_balance: 20.20,
+    current_score: 0,
+    current_playerInfo: "?"
 }
 
 // store PlayerObject objects here
@@ -77,10 +78,11 @@ var userArray = [];
 // define a constructor to create player objectsPlayerFirstName
 // this object gets used to pass up to mongo, but when we get the mongo
 // data back and store in the local array, it has the added primary key added
-var PlayerObject = function(pFirstName, pLastName, pBalance) {
- this.MongoFirstName = pFirstName;
- this.MongoLastName = pLastName;
- this.MongoBalance = (pBalance * 10)/10;
+var PlayerObject = function(pName, pKnownVal, pGuess, pScore) {
+ this.MongoName = pName;
+ this.MongoKnownVal = pKnownVal;
+ this.MongoGuess = pGuess;
+ this.MongoScore = pScore;
 }
 
 function createList() {
@@ -94,10 +96,9 @@ function createList() {
         // this code has to be in the getJSON callback function, since it uses data that is not valid until it completes
         $('#playerul').empty(); // don't want to keep adding old li s to old li s
         userArray.forEach(function(element) {
-            var fullName = element.MongoFirstName + "_" + element.MongoLastName
+            var playerInfo = element.MongoName + "_" + element.MongoKnownVal + "_" + element.MongoGuess
             $('#playerul').append('<li><a data-transition="pop" class="onePlayer" data-parm=' + 
-                fullName + ' href="#PickBet" > Pick your bet size.  ' + 
-                fullName + ' </a></li>' );
+                playerInfo + ' </a></li>' );
         });
     $('#playerul').listview('refresh');
     });
@@ -107,7 +108,7 @@ function createList() {
 function addNewUser(){
     // ajax call to mongo
     // Use AJAX to post the object to our adduser service
-    var newUser = new PlayerObject( $('#firstName').val(), $('#lastName').val(), 10)
+    var newUser = new PlayerObject( $('#name').val(), $('#knownVal').val(), $('#guess').val())
     $.ajax({
         type: 'POST',
         data: newUser,
@@ -116,9 +117,10 @@ function addNewUser(){
     }).done(function( response ) {
         if (response.msg === '') {
             // Clear the form inputs
-            $('#firstName').val('');
-            $('#lastName').val('');
-            document.location.href = "#pickplayer"; // move the html back to the pickplayer sub-page
+            $('#name').val('');
+            $('#knownVal').val('');
+            $('#guess').val('');
+            document.location.href = "#playerlist"; // move the html back to the pickplayer sub-page
         }
         else {
             // If something goes wrong, alert the error message that our service returned
@@ -128,65 +130,70 @@ function addNewUser(){
 };
 
 function buttonClicked() {
-    state.current_balance = GetNewBalance(state.current_balance);  // run one cycle of the game
-    (document.getElementById("balance")).innerText = (state.current_balance).toString();
-    if(state.current_balance <= 0) {
-        (document.getElementById("ButtonBet")).style.visibility = 'hidden';
-        setCurrent_index(state.current_fullName); // shouldn't need to do this, as the  state.current_index should still be accurate
-        state.currentFullName = ""; 
-        state.current_balance = userArray[state.current_index].PlayerBalance = 0.0;
+    state.current_score = GetNewBalance(state.current_score);  // run one cycle of the game
+    (document.getElementById("score")).innerText = (state.current_score).toString();
+    if(state.current_score <= 0) {
+        //(document.getElementById("ButtonBet")).style.visibility = 'hidden';
+        setCurrent_index(state.current_playerInfo); // shouldn't need to do this, as the  state.current_index should still be accurate
+        state.currentPlayerInfo = ""; 
+        state.current_score = userArray[state.current_index].playerScore = 0.0;
         // delete them from Mongo rigth here
         deleteuser();
         document.location.href = "index.html#LosePage";  // take player to lose page
     }
-    if(state.current_balance >= 20.0) {
-        state.current_balance = userArray[state.current_index].PlayerBalance = 10.0;  // set player back to $10
+    if(state.current_score >= 20.0) {
+        state.current_score = userArray[state.current_index].playerScore = 0;  // set player back to 0
         document.location.href = "index.html#WinPage";
     }
 };
 
 function GetNewBalance(balance) {
- var dice = [];
+ /*var dice = [];
  RollDice(dice);
  var dice1txt = "images/dice-" + dice[0] + ".jpg";
  var dice2txt = "images/dice-" + dice[1] + ".jpg";
  document.getElementById("image1").src = dice1txt;
- document.getElementById("image2").src = dice2txt;
- if (dice[0] == dice[1] || dice[0] + dice[1] == 7 || dice[0] + dice[1] == 11) {
-     balance = balance + state.current_betAmount;
+ document.getElementById("image2").src = dice2txt;*/
+    var score = 0;
+    var avg = userArray[pointer].MongoKnownVal;
+    var sum = avg.reduce(function(a+b) { return a + b; }, 0);
+ if (sum == userArray[pointer].MongoGuess)
+ {
+     score = score + 10;
+     score = score + state.current_score;
      (document.getElementById("status")).innerText = "You Win!";
  }
  else {
-     balance = balance - state.current_betAmount;
+     score = state.current_score;
      (document.getElementById("status")).innerText = "You Lost!";
  }
 
  
- var turnCount = (document.getElementById("turnCount")).innerText;
+ /*var turnCount = (document.getElementById("turnCount")).innerText;
  var turnCountInt = parseInt(turnCount);
  turnCountInt++;
- (document.getElementById("turnCount")).innerText = turnCountInt;
+ (document.getElementById("turnCount")).innerText = turnCountInt;*/
 
- return balance;
+ return score;
 }
 
-function RollDice(dice) {
+/*function RollDice(dice) {
  dice[0] = Math.floor((Math.random() * 6) + 1);
  dice[1] = Math.floor((Math.random() * 6) + 1);
 }  
-
+*/
 function quit(){
-        var playerBalance = parseInt(state.current_balance);
+        var playerScore = parseInt(state.current_score);
     var userID = userArray[state.current_index]._id;
     
     $.ajax({
     type: 'PUT',
-    url: '/users/updateuser/' + userID + '*' + playerBalance  // passsing 2 arguements as one
+    url: '/users/updateuser/' + userID + '*' + playerScore  // passsing 2 arguements as one
     }).done(function( response ) {
       
       if (response.msg === '') {
         
-            document.location.href = "#pickplayer"; // move the html back to the pickplayer sub-page
+            document.location.href = "#playerlist"; // move the html back to the pickplayer sub-page
         }
         else {
             // If something goes wrong, alert the error message that our service returned
@@ -215,10 +222,10 @@ function quit(){
     document.location.href = "#Home";
 }*/
 
-function setCurrent_index(fullName) {
+function setCurrent_index(playerInfo) {
     var pointer = 0;
     userArray.forEach(function(element) {
-        if( (userArray[pointer].MongoFirstName + "_" + userArray[pointer].MongoLastName) == fullName )  {
+        if( (userArray[pointer].MongoName + "_" + userArray[pointer].MongoKnownVal + "_" + userArray[pointer].MongoGuess) == playerInfo )  {
             state.current_index = pointer;
             return;
         }
@@ -231,7 +238,7 @@ function setCurrent_index(fullName) {
 
 
 function deleteuser() {
-    setCurrent_index(state.current_fullName)
+    setCurrent_index(state.current_playerInfo)
     var userID = userArray[state.current_index]._id;
     // pass up the full name, deal with splitting on the server
        $.ajax({
